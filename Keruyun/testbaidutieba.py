21d2a1a74887c4a665662eb8b1d15923598ac52f
zjb19911005 <397563682@qq.com>
2016-03-02 12:48:10 +0800

    增加tiebademo基础文件第一版

---
+++ Keruyun/testbaidutieba.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+from selenium import webdriver
+
+browser=webdriver.Firefox()
+browser.get("https://tieba.baidu.com")
+try:
+    browser.maximize_windows()
+except:
+    print 'Can not max'
+browser.implicitly_wait(3)
+#browser.find_element_by_class_name('u_login').click()
+browser.find_elements_by_link_text("登录").click()
+browser.implicitly_wait(3)
+browser.find_element_by_xpath("//div[@id='TANGRAM__PSP_8__userName']").clear()
+browser.find_element_by_xpath("//div[@id='TANGRAM__PSP_8__userName']").send_keys('zjb19911005')
+browser.close()
