8f0a9b6665d992e2d5ddf0ab3e70c7ea6a7096e1
Junior.Zhu <397563682@qq.com>
2016-03-03 14:22:59 +0800

    百度贴吧登陆

--- Keruyun/testbaidutieba.py
+++ Keruyun/testbaidutieba.py
@@ -1,16 +1,32 @@
 #coding=utf-8
 from selenium import webdriver
-
+from selenium.webdriver.common.action_chains import ActionChains
 browser=webdriver.Firefox()
 browser.get("http://tieba.baidu.com/")
+browser.implicitly_wait(4)
+print "设置浏览器大小啦"
 try:
-    browser.maximize_windows()
+#    browser.maximize_window()
+    browser.set_window_size(800,600)
 except:
-    print 'Can not max'
+    print '抱歉,设置失败了'
 browser.implicitly_wait(3)
-browser.find_element_by_class_name('u_login').click()
-#browser.find_elements_by_link_text("登录").click()
-browser.implicitly_wait(3)
-browser.find_element_by_xpath("//div[@id='TANGRAM__PSP_8__userName']").clear()
-browser.find_element_by_xpath("//div[@id='TANGRAM__PSP_8__userName']").send_keys('zjb19911005')
-browser.close()
+nowhandle=browser.current_window_handle
+print '开始操作登陆贴吧了'
+browser.find_element_by_xpath("//li[@class='u_login']").click()
+
+allhandles=browser.window_handles
+for handle in allhandles:
+    if handle!=nowhandle:
+        browser.switch_to.window(handle)
+        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__userName']").send_keys('123456')
+        browser.implicitly_wait(3)
+        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__password']").send_keys('zhu2311829')
+        browser.implicitly_wait(3)
+        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__memberPass']").click()
+        browser.implicitly_wait(2)
+        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__submit']").submit()
+        browser.implicitly_wait(2)
+
+#browser.quit()
+#browser.close()
