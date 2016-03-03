8ec64abfbb27acbdd915f7c5ad08d8d4fbe357e6
zjb19911005 <397563682@qq.com>
2016-03-03 18:24:53 +0800

    百度贴吧登陆

--- Keruyun/testbaidutieba.py
+++ Keruyun/testbaidutieba.py
@@ -1,32 +1,32 @@
 #coding=utf-8
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 browser=webdriver.Firefox()
 browser.get("http://tieba.baidu.com/")
-browser.implicitly_wait(4)
-print "设置浏览器大小啦"
-try:
-#    browser.maximize_window()
-    browser.set_window_size(800,600)
-except:
-    print '抱歉,设置失败了'
-browser.implicitly_wait(3)
-nowhandle=browser.current_window_handle
+browser.maximize_window()
 print '开始操作登陆贴吧了'
 browser.find_element_by_xpath("//li[@class='u_login']").click()
+browser.implicitly_wait(3)
 
-allhandles=browser.window_handles
-for handle in allhandles:
-    if handle!=nowhandle:
-        browser.switch_to.window(handle)
-        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__userName']").send_keys('123456')
-        browser.implicitly_wait(3)
-        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__password']").send_keys('zhu2311829')
-        browser.implicitly_wait(3)
-        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__memberPass']").click()
-        browser.implicitly_wait(2)
-        browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__submit']").submit()
-        browser.implicitly_wait(2)
+#browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__userName']").send_keys('123456')
+#username=browser.find_element_by_id('passport-login-pop').find_element_by_id('TANGRAM__PSP_8__userName')
+browser.execute_script('$("TANGRAM__PSP_8__submit").click();')
+
+
+
+
+
+
+
+'''browser.implicitly_wait(3)
+browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__userName']").send_keys(Keys.TAB)
+browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__password']").send_keys('zhu2311829')
+browser.implicitly_wait(3)
+browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__memberPass']").click()
+browser.implicitly_wait(2)
+browser.find_element_by_xpath("//input[@id='TANGRAM__PSP_8__submit']").submit()
+browser.implicitly_wait(2)'''
 
 #browser.quit()
 #browser.close()
