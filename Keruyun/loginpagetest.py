eb657e25d8eca8862a1cb8feb2aa653fee6bc65a
Junior.Zhu <397563682@qq.com>
2016-03-11 18:06:52 +0800

    开始准备web自动化测试用例

--- Keruyun/loginpagetest.py
+++ Keruyun/loginpagetest.py
@@ -5,25 +5,14 @@ import unittest
 from selenium import webdriver
 
 
-class firstpagetest(unittest.TestCase):
+class logginpagetest(unittest.TestCase):
     def setUp(self):
         print "开始 登陆主界面的测试 ..........Start .................."
         self.browser=webdriver.Firefox()
         self.url='https://testsso.shishike.com/cas/login?service=http://testb.shishike.com/cas'
         print '现在将浏览器最大化'
         self.browser.maximize_window()#浏览器最大化用例
-    def test001falselogin(self):#错误账户登录
-        self.browser.get(self.url+'/')
-        try:
-            self.browser.find_element_by_xpath("//input[@id='loginId']").send_keys(data(self))
-            self.browser.implicitly_wait(5)
-        except:
-            self.browser.get_screenshot_as_file('D:\\selenium\\keruyun\\errorpic\\loginId.png')
-        self.browser.find_element_by_xpath("//input[@id='username']").send_keys('admin')
-        self.browser.implicitly_wait(5)
-        self.browser.find_element_by_xpath("//input[@id='password']").send_keys('a123456')
-        self.browser.implicitly_wait(5)
-        self.browser.find_element_by_xpath("//input[@id='captcha']").send_keys()
+
     def test002turelogin(self):#正确账户登录
         self.browser.get(self.url+'/')
         self.browser.find_element_by_xpath("//input[@id='loginId']").clear()
@@ -37,7 +26,8 @@ class firstpagetest(unittest.TestCase):
         self.browser.find_element_by_xpath("//input[@id='password']").clear()
         self.browser.implicitly_wait(5)
         self.browser.find_element_by_xpath("//input[@id='password']").send_keys('a123456')
-
+        self.browser.implicitly_wait(3)
+        self.browser.find_element_by_xpath("//")
 
     def tearDown(self):
         print'完成登陆主界面的测试...........END............'
