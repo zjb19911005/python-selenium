9c00790b70ef1369099d6dc9b3871e4a2b12117b
zjb19911005 <397563682@qq.com>
2016-03-11 21:43:40 +0800

    添加菜品理由作废

--- Keruyun-OnMind/TestSuite.py
+++ Keruyun-OnMind/TestSuite.py
@@ -3,14 +3,14 @@ __author__ = 'Junior'
 import unittest
 
 from 登录与登出检查 import Logandlogout
-
+from 菜品作废理由设置 import test
 
 
 
 def suite():
     return unittest.makeSuite(Logandlogout,'test')
-# def suite2():
-#     return unittest.makeSuite(secondpagetestcase,'test')
+def suite2():
+    return unittest.makeSuite(test,'test')
 if __name__=='__main__':
     runner=unittest.TextTestRunner()
     runner.run(suite())
