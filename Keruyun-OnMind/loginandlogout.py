8e7a54514c38545be770db300b835ec0620876ca
zjb19911005 <397563682@qq.com>
2016-03-11 21:23:10 +0800

    第一条登录登出的用例

--- Keruyun-OnMind/loginandlogout.py
+++ Keruyun-OnMind/loginandlogout.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class Loginandlogout(unittest.TestCase):
+class LoginLogout(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -15,7 +15,7 @@ class Loginandlogout(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_loginandlogout(self):
+    def test_login_logout(self):
         driver = self.driver
         driver.get(self.base_url + "/cas/login?service=http://testb.shishike.com/cas")
         driver.find_element_by_id("username").clear()
@@ -25,16 +25,15 @@ class Loginandlogout(unittest.TestCase):
         driver.find_element_by_id("loginId").clear()
         driver.find_element_by_id("loginId").send_keys("4881")
         driver.find_element_by_id("captcha").clear()
-        driver.find_element_by_id("captcha").send_keys("ww")
+        driver.find_element_by_id("captcha").send_keys("w")
         driver.find_element_by_name("submit1").click()
-        driver.find_element_by_css_selector("div.user-info").click()
+        driver.find_element_by_css_selector("li[title=\"admin\"]").click()
         driver.find_element_by_id("logout").click()
         driver.find_element_by_link_text(u"确定").click()
         driver.find_element_by_id("username").clear()
         driver.find_element_by_id("username").send_keys("admin")
         driver.find_element_by_id("password").clear()
         driver.find_element_by_id("password").send_keys("a123456")
-        driver.implicitly_wait(10)
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
