c41ac929f5f01cc0e1e97823381952028c9d1121
zjb19911005 <397563682@qq.com>
2016-03-12 20:53:16 +0800

    Wxpython替换为Tkinter

--- Keruyun-OnMind/TkinterUI.py
+++ Keruyun-OnMind/TkinterUI.py
@@ -7,12 +7,13 @@ import  urllib
 
 
 root =tk.Tk()
-root.title(u'WEB自动化')
-tk.Label(root,fg = 'red',bg = 'blue',text = 'Hello I am Tkinter').pack()
+# bm= tk.BitmapImage(file='D:\self.bmp')
+tk.Label(root,fg = 'red',bg = 'blue',text = 'Hello I am Tkinter',width=100,height=50).pack()
 tk.Label(root,fg = 'red',bg = '#FF00FF',text = 'Hello I am Tkinter').pack()
+def helloButton():
+    print 'Hello world'
 
-
-
+tk.Button(root,text='Hello button',command=helloButton).pack()
 
 
 #进入消息循环
