27b4f02cc2f97188b0d80d3c3f75f59c3afb6f96
Junior.Zhu <397563682@qq.com>
2016-03-12 20:42:35 +0800

    Wxpython替换为Tkinter

---
+++ Keruyun-OnMind/TkinterUI.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+import Tkinter as tk
+import sys
+import os
+
+import  urllib
+
+
+root =tk.Tk()
+root.title(u'WEB自动化')
+tk.Label(root,fg = 'red',bg = 'blue',text = 'Hello I am Tkinter').pack()
+tk.Label(root,fg = 'red',bg = '#FF00FF',text = 'Hello I am Tkinter').pack()
+
+
+
+
+
+#进入消息循环
+root.mainloop()
