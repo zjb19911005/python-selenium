c6fc74e35f5beedcdcd406238f470071a4efc4f9
Junior.Zhu <397563682@qq.com>
2016-03-15 20:18:48 +0800

    添加自动化测试报告

--- Keruyun-OnMind/TestSuite.py
+++ Keruyun-OnMind/TestSuite.py
@@ -1,23 +1,35 @@
 # coding=utf-8
 __author__ = 'Junior'
 import unittest
+import HTMLTestRunner
+from caselists.changepassword import changepassword
+import time
 
-# from caselists import *
-#
-# def suite():
-#     return unittest.makeSuite(caselist, 'test')
-#
-#
-# if __name__ == '__main__':
-#     runner = unittest.TextTestRunner()
-#     runner.run(suite())
 
-import os
+if __name__ == '__main__':
+    testunit=unittest.TestSuite()
+    testunit.addTest(changepassword('test_'))
+    timestr = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
 
-caselist = os.listdir('/Users/Junior.Zhu/PycharmProjects/python-selenium/Keruyun-OnMind/caselists')
+filename ='D:\\result'+timestr+'.html'
+fp=file(filename,'wb')
+runner=HTMLTestRunner.HTMLTestRunner(
+stream=fp,
+title=u'自动化测试报告',
+description=u'自动化测试结果'
+)
+runner.run(testunit)
 
-for a in caselist:
-    s = a.split('.')[1:][0]  # 选取后缀为py的文件,从.后面开始切片
-    if s == 'py':
-        os.popen('python /Users/Junior.Zhu/PycharmProjects/python-selenium/Keruyun-OnMind/caselists%s1 >>/Users/Junior.Zhu/PycharmProjectslog.txt 2>&1' %a)
+
+
+
+# import os
+# import HTMLTestRunner
+# # caselist = os.listdir('/Users/Junior.Zhu/PycharmProjects/python-selenium/Keruyun-OnMind/caselists')#MAC下的文件路劲
+# caselist=os.system('C:\Users\Administrator\PycharmProjects\python-selenium\Keruyun-OnMind\caselists')#WINDOS路径
+# for a in caselist:
+#     s = a.split('.')[1:][0]  # 选取后缀为py的文件,从.后面开始切片
+#     if s == 'py':
+#         #WIN路径
+#         os.popen('python C:\Users\Administrator\PycharmProjects\python-selenium\Keruyun-OnMind\caselists%s1 >>/Users/Junior.Zhu/PycharmProjectslog.txt 2>&1' %a)
 
