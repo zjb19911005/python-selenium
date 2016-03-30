5ae88d3653ee052350a31b19c305dc990cdc3f3d
Junior.Zhu <397563682@qq.com>
2016-03-30 17:25:54 +0800

    添加py2exe

--- Keruyun-OnMind/py2exe.py
+++ Keruyun-OnMind/py2exe.py
@@ -6,17 +6,18 @@ __author__ = 'Junior'
 from distutils.core import setup
 import py2exe
 
-options = {"py2exe":
-            {   "compressed": 1,
-                "optimize": 2,
-                "bundle_files": 1   # <span style="color: rgb(255, 128, 0); line-height: 15px; white-space: pre; background-color: rgb(253, 253, 253); ">所有文件打包成一个exe文件</span>
-            }
-          }
+#  options = {"py2exe":
+#             {   "compressed": 1,
+#                 "optimize": 2,
+#                 "bundle_files": 1   # <span style="color: rgb(255, 128, 0); line-height: 15px; white-space: pre; background-color: rgb(253, 253, 253); ">所有文件打包成一个exe文件</span>
+#             }
+#           }
 setup(
     version = "0.0.1",
     description = "First Demo",
     name = "测试小工具",
-    options = options,
+    # options = options,
     zipfile = None, # 不生成zip库文件
-    console = [{"script": "Tkinter.py", "icon_resources": [(1, "Hello.ico")] }],
+    console=['TkinterUI.py']
+    # console = [{"script": "Tkinter.py", "icon_resources": [(1, "Hello.ico")] }],
     )
